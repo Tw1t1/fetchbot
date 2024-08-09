5675a161ffbbe17310e02330f33e5f14e120a5b2
YosefOwl <93843185+YosefOwl@users.noreply.github.com>
2024-08-09 20:39:21 +0300

    modify ball size range

--- grab_ball/grab_ball/ball_grabber.py
+++ grab_ball/grab_ball/ball_grabber.py
@@ -39,13 +39,13 @@ class GrabBall(Node):
         self.previous_position = None
         self.unchanged_position_count = 0
         self.position_unchanged = False
-        self.position_change_threshold = 0.1
+        self.position_change_threshold = 0.01
         self.position_range_min = 3.0
         self.position_range_max = 4.0
         
         # New variables for ball size analysis
-        self.ball_size_normal_min = 0.5
-        self.ball_size_normal_max = 0.9
+        self.ball_size_normal_min = 0.6 # 0.5
+        self.ball_size_normal_max = 0.9 
         self.ball_size_threshold_min = 0.4
         self.ball_size_threshold_max = 0.95
 
@@ -55,10 +55,10 @@ class GrabBall(Node):
 
     def position_callback(self, msg):
         try:
-            self.current_position = msg.data
+            self.current_position = round(msg.data, 3)
 
             if self.grab_process_state == GrabBallStatus.WAITING and self.current_position > 0.5 \
-                or self.current_position > 4.5:
+                or self.current_position > 4.0:
                 self.open_claw()
                 self.claw_state = 'open'
                 self.grab_process_state = GrabBallStatus.WAITING
@@ -68,9 +68,8 @@ class GrabBall(Node):
             
             if ball_in_grabe_position_range and self.grab_process_state == GrabBallStatus.CLOSING:
                 if self.previous_position is not None:
-                    self.current_position = round(self.current_position, 2)
-                    self.get_logger().info(f'position- previuse = {abs(self.current_position - self.previous_position)}')
-                    if abs(self.current_position - self.previous_position) < self.position_change_threshold:
+                    self.get_logger().info(f'Position - Previuse = {abs(self.current_position - self.previous_position)}')
+                    if abs(self.current_position - self.previous_position) <= self.position_change_threshold:
                         self.unchanged_position_count += 1
                         if self.unchanged_position_count >= 5:
                             self.position_unchanged = True
@@ -88,13 +87,10 @@ class GrabBall(Node):
     def ball_info_callback(self, msg):
         try:
             self.ball_info = msg
-
-            ball_in_left_bottom = self.ball_info.x < 0 and self.ball_info.y > 0
-            ball_in_right_bottom = self.ball_info.x > 0 and self.ball_info.y > 0
             
-            ball_size_status = self.check_ball_size(self.ball_info.z)
+            ball_size_status = self.check_ball_size(self.ball_info.x, self.ball_info.y, self.ball_info.z)
             
-            if (ball_in_left_bottom or ball_in_right_bottom) and ball_size_status == BallSizeStatus.NORMAL:
+            if ball_size_status == BallSizeStatus.NORMAL:
                 self.get_logger().info(f'Ball to grab: pos ({round(self.ball_info.x, 5)}, {round(self.ball_info.y, 5)}) , size {round(self.ball_info.z, 5)}')
 
                 grab_status = String()
@@ -119,15 +115,31 @@ class GrabBall(Node):
         except Exception as e:
             self.get_logger().error(f'Error in ball_info_callback: {str(e)}')
 
-    def check_ball_size(self, size):
-        if self.ball_size_normal_min <= size <= self.ball_size_normal_max:
+    def check_ball_size(self, x, y, size):
+
+        x_min, x_max = -0.21, 0.6
+        y_min, y_max = 0.40, 0.62
+        size_min, size_max = 0.69, 0.87
+        # ball_in_left_bottom = self.ball_info.x <= x_min and self.ball_info.y <= y_max
+        # ball_in_right_bottom = self.ball_info.x <= x_max and self.ball_info.y < 0
+        
+        if x_min <= x <= x_max and y_min <= y <= y_max and size_min <= size <= size_max:
             return BallSizeStatus.NORMAL
-        elif self.ball_size_threshold_min <= size < self.ball_size_normal_min or self.ball_size_normal_max < size <= self.ball_size_threshold_max:
+        elif (-0.21 <= x <= 0.6) and (0.0 <= y <= 0.93) and (0.39 <= size <= 0.97):
             self.get_logger().warn(f'Ball size {size} is outside normal range but within thresholds')
             return BallSizeStatus.SUSPICIOUS
         else:
             self.get_logger().error(f'Invalid ball size detected: {size}')
             return BallSizeStatus.INVALID
+        
+        # if self.ball_size_normal_min <= size <= self.ball_size_normal_max:
+        #     return BallSizeStatus.NORMAL
+        # elif self.ball_size_threshold_min <= size < self.ball_size_normal_min or self.ball_size_normal_max < size <= self.ball_size_threshold_max:
+        #     self.get_logger().warn(f'Ball size {size} is outside normal range but within thresholds')
+        #     return BallSizeStatus.SUSPICIOUS
+        # else:
+        #     self.get_logger().error(f'Invalid ball size detected: {size}')
+        #     return BallSizeStatus.INVALID
 
     def close_claw(self):
         self.claw_cmd.data = 'close'
