618d5e2ac05a1d457aa6b4537d4d9bfaeb9eb3e2
YosefOwl <shalomfall@gmail.com>
2024-09-01 13:00:16 +0300

    modify launch and config file

--- grab_ball/launch/grab_ball.launch.py
+++ grab_ball/launch/grab_ball.launch.py
@@ -3,7 +3,6 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 
-
 import os
 from ament_index_python.packages import get_package_share_directory
 
@@ -14,57 +13,32 @@ def generate_launch_description():
     params_file_dec = DeclareLaunchArgument(
         'params_file',
         default_value=os.path.join(get_package_share_directory('grab_ball'),'config','grab_ball_params_robot.yaml'),
-        description='Full path to params file for all ball_follow nodes.')
+        description='Full path to params file for all grab_ball nodes.')
 
 
 
-    model_file = LaunchConfiguration('params_file')
+    model_file = LaunchConfiguration('model_file')
     model_file_dec = DeclareLaunchArgument(
-        'params_file',
+        'model_file',
         default_value=os.path.join(get_package_share_directory('grab_ball'),'distance_models','rf_dist_model_0.3m_calib_graspel.joblib'),
         description='Full path to distance model file for grab_ball node.')
     
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    use_sim_time_dec = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='false',
-        description='Enables sim time for the follow node.')
     
-
-    cmd_vel_topic = LaunchConfiguration('cmd_vel_topic')
-    cmd_vel_topic_dec = DeclareLaunchArgument(
-        'cmd_vel_topic',
-        default_value='/grab_ball_follow_ball_inhibitore',
-        description='The name of the output command vel topic.')
-
-
     claw_controller_node = Node(
             package='grab_ball',
             executable='claw_controller',
             parameters=[params_file],
          )
-
-    locomotion_controller_node = Node(
-            package='grab_ball',
-            executable='locomotion_controller',
-            parameters=[params_file],
-            remappings=[('/cmd_vel', cmd_vel_topic)],
-         )
-
+    
     grab_ball_node = Node(
             package='grab_ball',
             executable='grab_ball',
-            parameters=[params_file, {'use_sim_time': use_sim_time}],
+            parameters=[params_file],
          )
 
-
     return LaunchDescription([
-
         params_file_dec,
-        use_sim_time_dec,
-        cmd_vel_topic_dec,
+        model_file_dec,
         claw_controller_node,
-        locomotion_controller_node,
         grab_ball_node
-
     ])
