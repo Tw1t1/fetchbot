7435c940fada7d58db77c674ef82584f53760225
Tw1t1 <Ben.shervi@gmail.com>
2024-09-03 17:34:21 +0300

    updated inhibitor & suppressor logic (removed redundant  parts)

--- suppressor_inhibitor/suppressor_inhibitor/suppressor.py
+++ suppressor_inhibitor/suppressor_inhibitor/suppressor.py
@@ -2,7 +2,7 @@ from rclpy.node import Node
 import time
 
 class SuppressorNode(Node):
-    def __init__(self, name, sub1_msg_type, sub1_topic_name, sub2_msg_type, sub2_topic_name, pub_msg_type, pub_topic_name, delay=0.5, publish_function=None):
+    def __init__(self, name, sub1_msg_type, sub1_topic_name, sub2_msg_type, sub2_topic_name, pub_msg_type, pub_topic_name, delay=0.5):
         super().__init__(name)
         self.subscriber1 = self.create_subscription(
             sub1_msg_type,
@@ -18,22 +18,12 @@ class SuppressorNode(Node):
         )
         self.publisher = self.create_publisher(pub_msg_type, pub_topic_name, 10)
         self.subscriber1_timer = time.time()
-        self.subscriber1_msg = None
-        self.subscriber2_msg = None
         self.delay = delay
-        self.publish_function = publish_function
 
     def subscriber1_callback(self, msg):
-        self.subscriber1_msg = msg
         self.subscriber1_timer = time.time()
-        if self.publish_function is not None:
-            self.subscriber1_msg = self.publish_function(self.subscriber1_msg)
-        self.publisher.publish(self.subscriber1_msg)
-
+        self.publisher.publish(msg)
 
     def subscriber2_callback(self, msg):
-        self.subscriber2_msg = msg
         if time.time() - self.subscriber1_timer > self.delay:
-            if self.publish_function is not None:
-                self.subscriber2_msg = self.publish_function(self.subscriber2_msg)
-            self.publisher.publish(self.subscriber2_msg)
+            self.publisher.publish(msg)
