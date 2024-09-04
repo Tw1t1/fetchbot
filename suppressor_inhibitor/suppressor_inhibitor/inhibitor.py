cc476bccfac9901e89fe1ad1519e85eb6672a34e
Tw1t1 <Ben.shervi@gmail.com>
2024-09-04 12:28:09 +0300

    update inhibitor for layer2-3 functions

--- suppressor_inhibitor/suppressor_inhibitor/inhibitor.py
+++ suppressor_inhibitor/suppressor_inhibitor/inhibitor.py
@@ -2,7 +2,7 @@ from rclpy.node import Node
 import time
 
 class InhibitorNode(Node):
-    def __init__(self, name, sub1_msg_type, sub1_topic_name, sub2_msg_type, sub2_topic_name, pub_msg_type, pub_topic_name, delay=0.5):
+    def __init__(self, name, sub1_msg_type, sub1_topic_name, sub2_msg_type, sub2_topic_name, pub_msg_type, pub_topic_name, delay=0.5, function=None):
         super().__init__(name)
         self.subscriber1 = self.create_subscription(
             sub1_msg_type,
@@ -19,9 +19,14 @@ class InhibitorNode(Node):
         self.publisher = self.create_publisher(pub_msg_type, pub_topic_name, 10)
         self.subscriber1_timer = time.time()
         self.delay = delay
+        self.function = function
 
     def subscriber1_callback(self, msg):
-        self.subscriber1_timer = time.time()
+        if self.function is None:   # If function is None, inhibit when message is received
+            self.subscriber1_timer = time.time()
+        elif self.function(msg):    # If function is not None, inhibit when function returns True
+            self.subscriber1_timer = time.time()
+        # If function is not None and function returns False, do not inhibit
 
     def subscriber2_callback(self, msg):
         if time.time() - self.subscriber1_timer > self.delay:
