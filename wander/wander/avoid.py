400dcdda18cc3059cee2fffe55a20ff0e1bc84cd
Tw1t1 <Ben.shervi@gmail.com>
2024-09-07 23:00:32 +0300

    update avoid logic

--- wander/wander/avoid.py
+++ wander/wander/avoid.py
@@ -20,7 +20,6 @@ class AvoidNode(Node):
         self.current_force = Force()
         self.current_heading = Heading()
 
-        self.random_factor = 0.3  # Random factor for combining heading and force
         self.significant_force = 0.5 # Adjust as needed
         self.collition_force = 10000 # Adjust as needed
 
@@ -41,6 +40,7 @@ class AvoidNode(Node):
     
         elif self.current_force.magnitude > self.significant_force:
             # Significant force, calculate new heading
+            self.current_heading.distance = 1.0 if self.current_heading.distance == 0 else self.current_heading.distance
             heading_x = self.current_heading.distance * math.cos(self.current_heading.angle)
             heading_y = self.current_heading.distance * math.sin(self.current_heading.angle)
             
@@ -48,16 +48,32 @@ class AvoidNode(Node):
             force_x = force_magnitude * math.cos(self.current_force.direction)
             force_y = force_magnitude * math.sin(self.current_force.direction)
 
-            average_x = self.random_factor * heading_x + (1 - self.random_factor) * force_x
-            average_y = self.random_factor * heading_y + (1 - self.random_factor) * force_y
+            force_factor = self.calculate_force_factor()
+            average_x = force_factor * force_x + (1 - force_factor) * heading_x
+            average_y = force_factor * force_y + (1 - force_factor) * heading_y
 
             new_heading.distance = math.sqrt(average_x**2 + average_y**2)
             new_heading.angle = math.atan2(average_y, average_x)
+            self.get_logger().info(f'heading: {self.current_heading.distance:.3f}, {self.current_heading.angle:.3f}')
+            self.get_logger().info(f'new_heading: {new_heading.distance:.3f}, {new_heading.angle:.3f}')
+            self.get_logger().info(f'force: {self.current_force.magnitude:.3f}, {self.current_force.direction:.3f}, {force_factor:.3f}')
+            self.get_logger().info('')
+
         else:
             # No significant force, maintain current heading
             new_heading = self.current_heading
 
         self.publisher.publish(new_heading)
+
+
+    def calculate_force_factor(self):
+        # Clamp the input value to a maximum of 3.0
+        clamped_value = min(self.current_force.magnitude, 3.0)
+        
+        # Map the range 0.5-3.0 to 0.1-1.0
+        mapped_value = 0.1 + (clamped_value - 0.5) * (0.9 / 2.5)
+        
+        return mapped_value    
 
 
 def main(args=None):
